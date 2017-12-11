58fd0e0ec2702aac8d5189f0b9088e6bb0677742
dannychua <decflow@gmail.com>
2017-12-11 03:35:32 -0500

    Edit api/app server ip

--- config/server.js
+++ config/server.js
@@ -1,8 +1,6 @@
-var ip = require('ip')
-
-module.exports.APP_SERVER_HOST = ip.address();
+module.exports.APP_SERVER_HOST = '104.196.151.14';
 module.exports.APP_SERVER_PORT = 3000;
-module.exports.API_SERVER_HOST = ip.address();
+module.exports.API_SERVER_HOST = '104.196.151.14';
 module.exports.API_SERVER_PORT = 4000;
 
 module.exports.MYSQL = {
